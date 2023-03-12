9e9e4f0de0e7949599c469cac9b0ff960b3ddcc1
KenStarry <starrycodes@gmail.com>
2023-03-13 00:11:56 +0300

    Compose for web introduction

--- Introduction/Introduction/src/jsMain/kotlin/Main.kt
+++ Introduction/Introduction/src/jsMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.href
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
@@ -11,9 +13,33 @@ fun main() {
     //  this is the main node that compose will use to handle its DOM tree
     renderComposable(rootElementId = "root") {
 
+        Div(
+            {
+                style {
+                    backgroundColor(Color.aquamarine)
+                    padding(25.px)
+                }
+            }
+        ) {
+
+            Input(type = InputType.Text) {
+
+            }
+
+            //  specifying a link
+            A(
+                attrs = {
+                    href("https://github.com")
+                }
+            )
+
+        }
+
         Div({
             style {
                 backgroundColor(Color.aliceblue)
+                width(500.px)
+                height(500.px)
                 padding(25.px)
             }
         }) {
